6f288d92d271063ea216bc144cb258d8e6b13448
Censera <Censera@proton.me>
2025-12-07 01:08:19 +0100

    Arithmetics

---
+++ arithmetics/main.py
@@ -0,0 +1,15 @@
+x = 10
+y = 5
+
+add = x + y
+sub = x - y
+mul = x * y
+div = x / y
+mod = x % (y - 1)
+
+print(x, "+", y, "=", add)
+print(x, "-", y, "=", sub)
+print(x, "*", y, "=", mul)
+print(x, "/", y, "=", div)
+print(x, "%", y+1, "=", mod)
+
